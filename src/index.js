6d2519d6c8644589b0922e6ac370053200681d50
kiddspazz <travis@travish.com>
2021-07-01 17:45:30 -0600

    standalone javascript project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 import perlinNoise from "./modules/perlin.js";
 import { outlineCircle, solidCircle, Color } from "./modules/drawing.js";
 
-function startApp() {
-  const canvas1 = document.getElementById('canvas1');
-  const W = canvas1.width = 512;
-  const H = canvas1.height = W;
-  const ctx = canvas1.getContext("2d");
+const makePerlinNoiseCanvas = ({ width, height }) => {
+  const canvas = document.createElement('canvas');
+  const contentDiv = document.getElementById('box');
+  canvas.width = width;
+  canvas.height = height;
+
+  return canvas;
+};
+
+const makeHeightGaugeCanvas = ({ width, height }) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style = "border: 1pt solid black";
+
+  return canvas;
+};
+
+export const renderPerlinNoiseInElement = (parentContainerId) => {
+  const parentContainer = document.getElementById(parentContainerId);
+  const W = 512;
+  const H = 512;
   const RADIUS = W/3;
   const STEPS = 360;
   let step = 0;
 
-  const canvas2 = document.getElementById('canvas2');
-  canvas2.width = 255;
-  canvas2.height = 12;
+  const heightGaugeCanvas = makeHeightGaugeCanvas({ width: 255, height: 12 });
+
+  const span = document.createElement('span');
+  const label = document.createTextNode('height gauge: ');
+  span.append(label);
+
+  const heightGaugeDiv = document.createElement('div');
+  heightGaugeDiv.appendChild(span);
+  heightGaugeDiv.appendChild(heightGaugeCanvas);
+
+  parentContainer.append(heightGaugeDiv);
+
+  const perlinNoiseCanvas = makePerlinNoiseCanvas({ width: W, height: H });
+  parentContainer.append(perlinNoiseCanvas);
+  const ctx = perlinNoiseCanvas.getContext("2d");
 
   let imageData = ctx.getImageData(0, 0, W, H);
   let data = imageData.data;
@@ -31,25 +60,26 @@ function startApp() {
     let y = Math.floor(findCircleY());
     solidCircle(x, y, 5, new Color(255, 255, 255).returnRGB(), ctx);
     let gN = elev[y * W + x];
-    const ctx2 = canvas2.getContext("2d");
-    ctx2.clearRect(0, 0, 255, 12);
+    const ctx2 = heightGaugeCanvas.getContext("2d");
+    ctx2.clearRect(0, 0, 512, 12);
     solidCircle(gN, 6, 5, new Color(40,40,40).returnRGB(), ctx2);
   }
 
   function printBackground() {
     for (let i = 0; i < data.length; i += 4) {
-      //    this puts a red grid overlay so you can see the unit grid
-      //    if ((i/4) % 32 === 0 || Math.floor((i/4)/512) % 32 === 0) {
-      //      data[i] = 255;
-      //      data[i + 1] = 0;
-      //      data[i + 2] = 0;
-      //    } else {
-      data[i] = data[i + 1] = data[i + 2] = elev[i/4];
-      //    }
+      let showRedGrid = false;
+      if (showRedGrid) {
+        if ((i/4) % 32 === 0 || Math.floor((i/4)/512) % 32 === 0) {
+          data[i] = 255;
+          data[i + 1] = 0;
+          data[i + 2] = 0;
+        }
+      } else {
+        data[i] = data[i + 1] = data[i + 2] = elev[i/4];
+      }
       data[i + 3] = 255;
     };
+
     ctx.putImageData(imageData, 0, 0);
   }
 }
-
-export default startApp;
